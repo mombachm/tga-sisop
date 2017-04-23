a45d6bb804e0771935164e3e47cab0992e77600a
Matheus <mombachm@gmail.com>
2017-04-23 18:30:02 -0300

    Upload Felipe's changes.

--- main.c
+++ main.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include "menu.h"
 
 struct Menu menuPrincipal;
+
+
 
 int main(int argc, char argv[]){
 	montaMenuPrincipal();
@@ -11,6 +16,9 @@ int main(int argc, char argv[]){
 
 	return EXIT_SUCCESS;
 }
+
+void sig_handler(int signo);
+
 
 /**
 Função que inicializa o menu principal, montando o título e seus menus.
@@ -38,21 +46,46 @@ void executaMenuPrincipal(){
     int resposta;
     unsigned int id;
 
+    if (signal(SIGINT, sig_handler) == SIG_ERR)
+        printf("\nNão consegui pegar o SIGINT\n");
+
     system("clear");
     resposta = exibeMenu(menuPrincipal);
     switch (resposta){
         case 1: //Web Browser
-
-            break;
+			printf("Digite o site que gostaria de visitar: \n");
+			char* s;
+			scanf("%s", s);
+			execl("/usr/bin/firefox", "firefox", s, NULL);
+			pid_t firefox = fork(); // não tá funcionando
+			printf("(executando, pid=%d)\n", firefox);
+			break;
         case 2: //Text Editor
-
-            break;
+			execl("/usr/bin/gedit", "gedit", NULL);
+			pid_t gedit = fork(); // não tá funcionando
+			printf("(executando, pid=%d)\n", gedit);
+			break;
         case 3: //Terminal
-
-            break;
+			system("gnome-terminal");
+			pid_t terminal = fork(); // não tá funcionando
+			printf("(executando, pid=%d)\n", terminal);
+			break;
         case 4: //Finalizar Processo
+			printf("Qual processo deseja terminar?\n");
+			printf("1) Web Browser\n");
+			printf("2) Text Editor\n");
+			printf("3) Terminal\n");
 
-            break;
+			int proc;
+			scanf("%d", &proc);
+			if (proc == 1)
+				kill(firefox, SIGTERM);
+			if (proc == 2)
+				kill(gedit, SIGTERM);
+			if (proc == 3)
+				kill(terminal, SIGTERM);
+
+			break;
         case 5: //Sair
 
             break;
@@ -65,3 +98,9 @@ void executaMenuPrincipal(){
     getchar();
     return executaMenuPrincipal();
 }
+
+void sig_handler(int signo) {
+	if (signo == SIGINT)
+		printf("Relebeu sinal.");
+}
+
